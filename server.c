ec072ee820a62147c611c505d179896e90a15898
= <=>
2022-12-22 13:19:03 -0600

    commit

--- server.c
+++ server.c
@@ -1,3 +1,14 @@
+// struct addrinfo {
+// int ai_flags; // AI_PASSIVE, AI_CANONNAME, etc.
+// int ai_family; // AF_INET, AF_INET6, AF_UNSPEC
+// int ai_socktype; // SOCK_STREAM, SOCK_DGRAM
+// int ai_protocol; // use 0 for "any"
+// size_t ai_addrlen; // size of ai_addr in bytes
+// struct sockaddr *ai_addr; // struct sockaddr_in or _in6
+// char *ai_canonname; // full canonical hostname
+// struct addrinfo *ai_next; // linked list, next node
+// };
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -85,7 +96,7 @@ int main(void) {
     printf("server: waotomg for connections...\n");
 
     while(1) {
-        //
+        //testing
     }
 
     
---
+++ talker.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+#define PORT "4590"
+
+int main(int argc, char*argv[]) {
+    int sockfd;
+    struct addrinfo *hints, *results, *p;
+    int rv;
+    int numbytes;
+
+    if (argc != 3) {
+        fprintf(stderr, "adjhflakjhdf");
+        exit(1);
+    }
+
+    memset(&hints, 0, sizeof hints);
+    hints->ai_family = AF_UNSPEC;
+    hints->ai_socktype = SOCK_DGRAM;
+
+    if ((rv = getaddrinfo(argv[1], PORT, &hints, &results)) != 0) {
+        fprintf(stderr, "getaddr info: %s\n", gai_strerror(rv));
+        return 1;
+    }
+
+    for (p = results; p != NULL; p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+            perror("talker: socket");
+            continue;
+        }
+
+        break;
+    }
+
+    if (p == NULL) {
+        fprintf(stderr, "talker: failed to bind socket");
+        return 2;
+    }
+
+    if ((numbytes = sendto(sockfd, argv[2], strlen(argv[2]), 0, p->ai_addr, p->ai_addrlen))) {
+        perror("talker: sendto");
+        exit(1);
+    }
+
+    freeaddrinfo(results);
+
+    printf("talker: sent %d bytes to %s\n", numbytes, argv[1]);
+	close(sockfd);
+
+    return 0;
+
+}
