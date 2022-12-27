f20cc2b744264067e4b2764af2bd87f2b95c28b7
= <=>
2022-12-27 12:55:06 -0600

    commit

--- server.c
+++ server.c
@@ -103,7 +103,7 @@ int main(void) {
     while(1) {
         //testing
         sin_size = sizeof(their_addr);
-        new_fd = accept(sockfd, (struct sockaddr * )&their_addr, &sin_size);
+        new_fd = accept(sockfd, (struct sockaddr*)&their_addr, &sin_size);
 
         if (new_fd == -1) {
             perror("accept");
